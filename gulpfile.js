20aeceed8cbc85195ad40ecd51bc2a6f20f5d742
Cole Peterson <Cole.Peterson@possible.com>
2016-11-23 19:36:03 +0000

    added content for every page. upped the font

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,44 @@
 var fileinclude = require('gulp-file-include'),
     gulp = require('gulp');
 
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
 gulp.task('fileinclude', function() {
-    gulp.src('**/*.html')
+    gulp.src([
+            '**/*.html',
+            '!built',
+            '!built/**',
+        ])
         .pipe(fileinclude({
             prefix: '@@',
             basepath: '@file'
         }))
         .pipe(gulp.dest('./built/'));
+});
+
+// create a task that ensures the `js` task is complete before
+// reloading browsers
+gulp.task('reloadme', [], function(done) {
+    browserSync.reload();
+    done();
+});
+
+// use default task to launch Browsersync and watch JS files
+gulp.task('serve', ['fileinclude'], function() {
+
+    // Serve files from the root of this project
+    browserSync.init({
+        server: {
+            baseDir: "./built/"
+        }
+    });
+
+
+    gulp.watch(['**/*.html',
+        '!built',
+        '!built/**'
+    ], ['fileinclude', 'reloadme']);
 });
 
 
